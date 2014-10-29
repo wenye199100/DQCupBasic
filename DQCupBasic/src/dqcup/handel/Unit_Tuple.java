93b52b3f1051accdace5f244f6025420c1270b8c
wenye199100 <two_ye@hotmail.com>
2014-10-29 20:52:38 +0800

    finish 查重
    完成查重，下一步正则匹配和修改

--- DQCupBasic/src/dqcup/handel/Unit_Tuple.java
+++ DQCupBasic/src/dqcup/handel/Unit_Tuple.java
@@ -14,36 +14,71 @@ public class Unit_Tuple {
 	
 	public Unit_Tuple(Tuple tuple)
 	{
+		//必须提取RUID
+		//tuples[][0] ruid
+		//tuples[][1] cuid
+		
 		CUID = tuple.getValue(1);
-		for(int i = 0; i < 15; i++)
-			tuples[0][i] = tuple.getValue(i + 1);
+		for(int i = 0; i < 16; i++)
+			tuples[0][i] = tuple.getValue(i);
 		n++;
 	}
 	
 	public void add(Tuple tuple) {
-		for(int i = 0; i < 15; i++)
-			tuples[n][i] = tuple.getValue(i + 1);
+		for(int i = 0; i < 16; i++)
+			tuples[n][i] = tuple.getValue(i);
 		n++;
 	}
 	
 	public void run() {
+		if(this.CUID == null)
+			return;
+		
 		Regex regex = new Regex(tuples[0][3]);
+	
 		String[] Content = new String[10];
-		int[][] line = new int[10][100];
-		for(int i = 0; i < 15; i++)
+		int Content_Num = 0;
+		
+		int line[][] = new int[10][100];
+		int[] line_num = new int[10];
+		for(int i = 2; i < 16; i++)
 		{
+			Content = new String[10];
+			Content_Num = 0;
+
+			for(int m = 0; m < 10; m++)
+				for(int n = 0; n < 100; n++)
+					line[m][n] = -1;
+				
+			line_num = new int[10];
 			boolean same = false;
 			for(int j = 0; j < n; j++)
 			{
-				for(int k = 0; k < Content.length; k++)
+				for(int k = 0; k < Content_Num; k++)
 				{
-					if(Content[k].equals(tuples[i][j]) )
+					same = false;
+					if(Content[k].equals(tuples[j][i]) )
 					{
-						line[][] = 
+						same = true;
+						line[k][line_num[k]++] = Integer.parseInt(tuples[j][0]); 
+						break;
 					}
 				}
+				if(!same)
+				{
+					
+					Content[Content_Num] =  tuples[j][i];
+					line[Content_Num][0] = Integer.parseInt(tuples[j][0]); 
+					line_num[Content_Num++] = 1;
+				}
 			}
+			int a = 1;
+			int b = 2;
+			a = a+b;
 		}
+		
+		
+		
 	}
 
 	
